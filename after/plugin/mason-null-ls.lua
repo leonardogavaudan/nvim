726324ed474bb8d1281f11c1c797bde6884330fd
leonardogavaudan <leonardogavaudan@gmail.com>
2023-10-24 21:43:52 +0200

    add eslint and prettier to formatting source

--- after/plugin/mason-null-ls.lua
+++ after/plugin/mason-null-ls.lua
@@ -7,6 +7,8 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.eslint,
   },
 })
 
