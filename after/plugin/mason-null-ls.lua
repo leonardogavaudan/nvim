82c5df9ae901a17610c5ecab3ca8896146b9f740
leonardogavaudan <leonardogavaudan@gmail.com>
2023-11-15 21:09:12 +0100

    Document mason-null-ls.lua

--- after/plugin/mason-null-ls.lua
+++ after/plugin/mason-null-ls.lua
@@ -1,23 +1,29 @@
-require("mason-null-ls").setup({
+local mason_null_ls = require("mason-null-ls")
+local null_ls = require("null-ls")
+
+-- Setting up mason-null-ls with specified formatters to ensure they are installed
+mason_null_ls.setup({
 	ensure_installed = { "black", "prettier", "eslint-d" }
 })
 
-local null_ls = require("null-ls")
-
+-- Configuring null-ls to use specific formatters
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.eslint,
+		null_ls.builtins.formatting.black,  -- Python formatter
+		null_ls.builtins.formatting.prettier, -- General purpose formatter
+		null_ls.builtins.formatting.eslint, -- JavaScript/TypeScript linter and formatter
 	},
 })
 
+-- Keymap for manually triggering formatting in normal mode
 vim.keymap.set("n", "<leader>m", function()
 	vim.lsp.buf.format()
 end)
 
+-- Auto format before saving file
 vim.api.nvim_create_autocmd("BufWritePre", {
 	callback = function()
 		vim.lsp.buf.format({ timeout_ms = 1000 })
 	end,
 })
+
