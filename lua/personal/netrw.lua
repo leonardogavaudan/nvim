526e6f75dff3284433f4d382e121c5ccc6491d2a
leonardogavaudan <leonardogavaudan@gmail.com>
2023-11-15 20:37:47 +0100

    reformat files

--- lua/personal/netrw.lua
+++ lua/personal/netrw.lua
@@ -1,14 +1,14 @@
 function OpenFileExternally()
-  -- Get the current file's path using Netrw's internal variable
-  local filepath = vim.fn.getline("."):gsub(" ", "\\ ")
-  -- Execute the open command with the full file path
-  vim.cmd('!open ' .. filepath)
+	-- Get the current file's path using Netrw's internal variable
+	local filepath = vim.fn.getline("."):gsub(" ", "\\ ")
+	-- Execute the open command with the full file path
+	vim.cmd('!open ' .. filepath)
 end
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "netrw",
-  callback = function()
-    vim.api.nvim_buf_set_keymap(0, 'n', '<leader>o', ':lua OpenFileExternally()<CR>', { noremap = true, silent = true })
-  end
+	pattern = "netrw",
+	callback = function()
+		vim.api.nvim_buf_set_keymap(0, 'n', '<leader>o', ':lua OpenFileExternally()<CR>',
+			{ noremap = true, silent = true })
+	end
 })
-
