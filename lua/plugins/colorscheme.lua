394f968e2cf3fd2c5042f342698cf7be4c9a357b
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-09-19 14:45:13 +0200

    Update plugins + switch to github colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -18,6 +18,24 @@ local vscode = {
 	end,
 }
 
+local github = {
+	"projekt0n/github-nvim-theme",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		require("github-theme").setup({})
+		vim.cmd("colorscheme github_dark_dimmed")
+	end,
+}
+
+local kanagawa = {
+	"rebelot/kanagawa.nvim",
+	branch = "master",
+	config = function()
+		vim.cmd("colorscheme kanagawa")
+	end,
+}
+
 return {
-	vscode,
+	github,
 }
