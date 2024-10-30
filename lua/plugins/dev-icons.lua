9f66e6dc77d34b6e36986706ba4dfdbd9efea146
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-10-30 09:16:59 +0100

    Enable dev-icons default icons

--- lua/plugins/dev-icons.lua
+++ lua/plugins/dev-icons.lua
@@ -1,4 +1,10 @@
 return {
   "nvim-tree/nvim-web-devicons",
   branch = "master",
+  config = function()
+    local web_devicons = require("nvim-web-devicons")
+    web_devicons.setup({
+      default = true,
+    })
+  end,
 }
