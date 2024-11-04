897b1b5e3994f9bc296d318ab9ad148778f74622
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-11-04 16:34:49 +0100

    Added mapping for moving view up and down 1 line

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -22,33 +22,33 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
 
 -- Get file path from git project root
 vim.keymap.set("n", "<leader>ph", function()
-	-- Get the full path of the current file
-	local file_path = vim.fn.expand("%:p")
+  -- Get the full path of the current file
+  local file_path = vim.fn.expand("%:p")
 
-	-- Get the root directory of the git repository
-	local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
-	if git_root == nil or git_root == "" then
-		print("Not a git repository")
-		return
-	end
+  -- Get the root directory of the git repository
+  local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+  if git_root == nil or git_root == "" then
+    print("Not a git repository")
+    return
+  end
 
-	-- Ensure the git root path ends with a slash
-	if string.sub(git_root, -1) ~= "/" then
-		git_root = git_root .. "/"
-	end
+  -- Ensure the git root path ends with a slash
+  if string.sub(git_root, -1) ~= "/" then
+    git_root = git_root .. "/"
+  end
 
-	-- Calculate the relative path from the git root to the current file
-	local relative_path = ""
-	if string.sub(file_path, 1, string.len(git_root)) == git_root then
-		relative_path = string.sub(file_path, string.len(git_root) + 1)
-	else
-		print("File is not inside git root")
-		return
-	end
+  -- Calculate the relative path from the git root to the current file
+  local relative_path = ""
+  if string.sub(file_path, 1, string.len(git_root)) == git_root then
+    relative_path = string.sub(file_path, string.len(git_root) + 1)
+  else
+    print("File is not inside git root")
+    return
+  end
 
-	-- Copy the relative path to the system clipboard
-	vim.fn.setreg("+", relative_path)
-	print("Copied relative path to clipboard: " .. relative_path)
+  -- Copy the relative path to the system clipboard
+  vim.fn.setreg("+", relative_path)
+  print("Copied relative path to clipboard: " .. relative_path)
 end, { noremap = true, silent = true })
 
 -- Paste without changing the register
@@ -58,5 +58,10 @@ vim.keymap.set("v", "<leader>d", '"_d', { noremap = true, silent = true })
 
 -- Change director to current in netrw
 vim.keymap.set("n", "<leader>cd", function()
-	vim.api.nvim_set_current_dir(vim.fn.expand("%:p:h"))
+  vim.api.nvim_set_current_dir(vim.fn.expand("%:p:h"))
 end)
+
+-- vim.keymap.set("n", ",", "jzz", { noremap = true, silent = true })
+-- vim.keymap.set("n", ".", "kzz", { noremap = true, silent = true })
+vim.keymap.set("n", ",", "<C-e>") -- Move view down one line
+vim.keymap.set("n", ".", "<C-y>") -- Move view up one line
