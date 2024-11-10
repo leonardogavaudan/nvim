ed24678792ba383d90c0384bdbb9388dfbd0cd8e
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-11-10 14:36:12 +0100

    Added vim-illuminate plugin

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,50 +1,66 @@
 return {
-  "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require("lspconfig")
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	"neovim/nvim-lspconfig",
+	"RRethy/vim-illuminate",
+	config = function()
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    -- LUA
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          runtime = {
-            version = "LuaJIT",
-            path = vim.split(package.path, ";"),
-          },
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            library = vim.api.nvim_get_runtime_file("", true), -- Neovim runtime files
-            checkThirdParty = false,
-          },
-          telemetry = {
-            enable = false,
-          },
-        },
-      },
-    })
+		-- Configure vim-illuminate
+		require("illuminate").configure({
+			delay = 100, -- delay in milliseconds before highlighting occurs
+		})
 
-    -- PYTHON
-    lspconfig.pyright.setup({
-      capabilities = capabilities,
-      on_attach = function()
-        vim.opt.softtabstop = 4
-        vim.opt.tabstop = 4
-        vim.opt.shiftwidth = 4
-      end,
-    })
+		-- LUA
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			on_attach = function(client, bufnr)
+				require("illuminate").on_attach(client)
+			end,
+			settings = { -- Lua settings
+				Lua = {
+					runtime = {
+						version = "LuaJIT",
+						path = vim.split(package.path, ";"),
+					},
+					diagnostics = {
+						globals = { "vim" },
+					},
+					workspace = {
+						library = vim.api.nvim_get_runtime_file("", true),
+						checkThirdParty = false,
+					},
+					telemetry = {
+						enable = false,
+					},
+				},
+			},
+		})
 
-    -- TYPESCRIPT
-    lspconfig.ts_ls.setup({
-      capabilities = capabilities,
-    })
+		-- PYTHON
+		lspconfig.pyright.setup({
+			capabilities = capabilities,
+			on_attach = function(client, bufnr)
+				require("illuminate").on_attach(client)
+				vim.opt.softtabstop = 4
+				vim.opt.tabstop = 4
+				vim.opt.shiftwidth = 4
+			end,
+		})
 
-    -- C
-    lspconfig.clangd.setup({
-      capabilities = capabilities,
-    })
-  end,
+		-- TYPESCRIPT
+		lspconfig.ts_ls.setup({
+			capabilities = capabilities,
+			on_attach = function(client, bufnr)
+				require("illuminate").on_attach(client)
+			end,
+		})
+
+		-- C
+		lspconfig.clangd.setup({
+			capabilities = capabilities,
+			on_attach = function(client, bufnr)
+				require("illuminate").on_attach(client)
+			end,
+		})
+	end,
 }
