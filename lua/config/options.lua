94e56dbf1601d77c6f72834345f30a1e675871cc
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-11-27 15:46:06 +0100

    Add options for debugging tabs and chars

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,6 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
+
+-- vim.opt.list = true -- allows the display of invisible characters
+-- vim.opt.listchars = { tab = "▸ ", space = "·" }
