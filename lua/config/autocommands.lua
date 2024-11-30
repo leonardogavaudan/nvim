bc930b6373a38a26b577cc89d7dbb2946d3ee7ad
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-11-30 21:22:17 +0100

    Highlight on yank

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -25,3 +25,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.lsp.buf.format({ async = false })
   end,
 })
+
+-- Highlight when yanking (copying) text, see `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
