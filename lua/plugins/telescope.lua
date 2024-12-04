f0ea5f07f700546334f8ac94506a22f42a28efe4
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-12-04 21:40:55 +0100

    replace tabs in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,23 +1,23 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-	},
-	config = function()
-		local telescope = require("telescope")
-		local builtin = require("telescope.builtin")
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    local telescope = require("telescope")
+    local builtin = require("telescope.builtin")
 
-		telescope.setup({
-			defaults = {
-				path_display = { "truncate" },
-			},
-		})
+    telescope.setup({
+      defaults = {
+        path_display = { "truncate" },
+      },
+    })
 
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-	end,
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+  end,
 }
