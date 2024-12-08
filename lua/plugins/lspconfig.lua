0fb756ebac20eb67bb99209c57f63baf3c4a8641
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2024-12-08 21:33:48 +0100

    Replace tabs with space

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,61 +1,61 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local lspconfig = require("lspconfig")
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lspconfig = require("lspconfig")
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-		-- LUA
-		lspconfig.lua_ls.setup({
-			capabilities = capabilities,
-			settings = {
-				Lua = {
-					runtime = {
-						version = "LuaJIT",
-						path = vim.split(package.path, ";"),
-					},
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						library = vim.api.nvim_get_runtime_file("", true),
-						checkThirdParty = false,
-					},
-					telemetry = {
-						enable = false,
-					},
-					format = {
-						enable = false,
-					},
-				},
-			},
-		})
+    -- LUA
+    lspconfig.lua_ls.setup({
+      capabilities = capabilities,
+      settings = {
+        Lua = {
+          runtime = {
+            version = "LuaJIT",
+            path = vim.split(package.path, ";"),
+          },
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false,
+          },
+          telemetry = {
+            enable = false,
+          },
+          format = {
+            enable = false,
+          },
+        },
+      },
+    })
 
-		-- PYTHON
-		lspconfig.pyright.setup({
-			capabilities = capabilities,
-			settings = {
-				python = {
-					formatting = {
-						provider = "autopep8", -- Example, use Black or others as needed
-					},
-				},
-			},
-		})
+    -- PYTHON
+    lspconfig.pyright.setup({
+      capabilities = capabilities,
+      settings = {
+        python = {
+          formatting = {
+            provider = "autopep8", -- Example, use Black or others as needed
+          },
+        },
+      },
+    })
 
-		-- TYPESCRIPT
-		lspconfig.ts_ls.setup({
-			capabilities = capabilities,
-			settings = {
-				format = {
-					tabSize = vim.opt.tabstop:get(),
-					insertSpaces = vim.opt.expandtab:get(),
-				},
-			},
-		})
+    -- TYPESCRIPT
+    lspconfig.ts_ls.setup({
+      capabilities = capabilities,
+      settings = {
+        format = {
+          tabSize = vim.opt.tabstop:get(),
+          insertSpaces = vim.opt.expandtab:get(),
+        },
+      },
+    })
 
-		-- C
-		lspconfig.clangd.setup({
-			capabilities = capabilities,
-		})
-	end,
+    -- C
+    lspconfig.clangd.setup({
+      capabilities = capabilities,
+    })
+  end,
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,21 +1,21 @@
 return {
-	"nvimtools/none-ls.nvim",
-	branch = "main",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  branch = "main",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
 
-		local sources = {
-			-- LUA
-			null_ls.builtins.formatting.stylua.with({
-				extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
-			}),
-			-- JS
-			null_ls.builtins.formatting.prettier,
-			-- PYTHON
-			null_ls.builtins.formatting.black,
-		}
+    local sources = {
+      -- LUA
+      null_ls.builtins.formatting.stylua.with({
+        extra_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+      }),
+      -- JS
+      null_ls.builtins.formatting.prettier,
+      -- PYTHON
+      null_ls.builtins.formatting.black,
+    }
 
-		null_ls.setup({ sources = sources })
-	end,
+    null_ls.setup({ sources = sources })
+  end,
 }
