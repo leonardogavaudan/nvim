18865a568a94250b277b2ad0087bb8d968b9c524
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-02-15 20:18:53 +0100

    Avante: default to using openai - gpt4o vibe shift is real

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -4,7 +4,7 @@ return {
 	lazy = false,
 	version = false, -- set this if you want to always pull the latest change
 	opts = {
-		provider = "claude",
+		provider = "openai",
 		claude = {
 			endpoint = "https://api.anthropic.com",
 			model = "claude-3-5-sonnet-20241022",
