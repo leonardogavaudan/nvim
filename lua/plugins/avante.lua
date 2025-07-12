28c98a62a4f14881d4d486382ed46c7995a0f2e1
Leonardo Gavaudan <leonardogavaudan@gmail.com>
2025-07-12 10:17:53 +0200

    udpate sonnet in avant config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,5 +1,13 @@
 return {
     "yetone/avante.nvim",
+    build = function()
+        -- conditionally use the correct build system for the current OS
+        if vim.fn.has("win32") == 1 then
+            return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        else
+            return "make"
+        end
+    end,
     event = "VeryLazy",
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
@@ -10,7 +18,7 @@ return {
         providers = {
             claude = {
                 endpoint = "https://api.anthropic.com",
-                model = "claude-3-7-sonnet-20250219",
+                model = "claude-sonnet-4-20250514",
             },
             openai = {
                 endpoint = "https://api.openai.com/v1",
@@ -23,15 +31,10 @@ return {
                 __inherited_from = "openai",
                 endpoint = "https://openrouter.ai/api/v1",
                 api_key_name = "OPENROUTER_API_KEY",
-                model = "google/gemini-2.5-flash-preview-05-20:thinking",
-                disable_tools = true,
+                model = "",
+                disable_tools = false,
                 reasoning = {
                     exclude = true,
-                },
-            },
-            ui = {
-                ask = {
-                    min_height = 3, -- minimum number of lines; adjust as you like
                 },
             },
         },
@@ -68,32 +71,40 @@ return {
             end,
         },
     },
-    build = "make",
     dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "stevearc/dressing.nvim",
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "zbirenbaum/copilot.lua",
+        --- The below dependencies are optional,
+        "echasnovski/mini.pick", -- for file_selector provider mini.pick
+        "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+        "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+        "ibhagwan/fzf-lua", -- for file_selector provider fzf
+        "stevearc/dressing.nvim", -- for input provider dressing
+        "folke/snacks.nvim", -- for input provider snacks
+        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua", -- for providers='copilot'
         {
+            -- support for image pasting
             "HakonHarnes/img-clip.nvim",
             event = "VeryLazy",
             opts = {
+                -- recommended settings
                 default = {
                     embed_image_as_base64 = false,
                     prompt_for_file_name = false,
                     drag_and_drop = {
                         insert_mode = true,
                     },
+                    -- required for Windows users
                     use_absolute_path = true,
                 },
             },
         },
         {
+            -- Make sure to set this up properly if you have lazy=true
             "MeanderingProgrammer/render-markdown.nvim",
             opts = {
-                file_types = { "Avante" },
+                file_types = { "markdown", "Avante" },
             },
             ft = { "markdown", "Avante" },
         },
