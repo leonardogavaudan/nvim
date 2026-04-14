8d4fae8b4435d149c0bf396f87de5b72f07384b2
Leonardo Gavaudan <leonardo.gavaudan@algolia.com>
2026-04-14 17:52:46 +0200

    telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,24 +8,34 @@ return {
     config = function()
         local telescope = require("telescope")
         local builtin = require("telescope.builtin")
+        local make_entry = require("telescope.make_entry")
 
         local function only_filename_entry_maker(entry)
-            local filename = entry.filename or entry.uri or entry.path or "[No file]"
-            if filename:match("^%w+://") then
-                filename = vim.uri_to_fname(filename)
+            local full_filename = entry.filename or entry.uri or entry.path or "[No file]"
+            if full_filename:match("^%w+://") then
+                full_filename = vim.uri_to_fname(full_filename)
             end
-            -- Get git root
+
+            local display_filename = full_filename
             local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
-            if git_root and filename:sub(1, #git_root) == git_root then
-                filename = filename:sub(#git_root + 2) -- +2 to remove the slash
+            if git_root and display_filename:sub(1, #git_root) == git_root then
+                display_filename = display_filename:sub(#git_root + 2)
             end
-            return {
+
+            local text = entry.text or ""
+            local ordinal = table.concat({ full_filename, tostring(entry.lnum or ""), tostring(entry.col or ""), text }, " ")
+
+            return make_entry.set_default_entry_mt({
                 value = entry,
-                ordinal = filename,
-                display = filename,
-                filename = filename,
+                ordinal = ordinal,
+                display = display_filename,
+                filename = full_filename,
                 lnum = entry.lnum,
-            }
+                col = entry.col,
+                text = text,
+                start = entry.start,
+                finish = entry.finish,
+            }, {})
         end
 
         telescope.setup({
